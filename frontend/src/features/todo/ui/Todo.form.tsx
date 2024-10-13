91960bc8c1d933a085bc83709f6c21bd5fbb3a3c
YouAreNotHere <malferety@gmail.com>
2024-10-13 18:20:16 +0500

    Реализовал alies

--- frontend/src/features/todo/ui/Todo.form.tsx
+++ frontend/src/features/todo/ui/Todo.form.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import '../../../app/App.css';
-import { useRequest } from '../../../shared/hooks/useRequest';
+import '@/app/App.css';
+import { useRequest } from '@/shared/hooks/useRequest';
 import {
   editTodo,
   changeEditedTodoId,
   deleteTodo,
   completeTodo,
-} from '../../../actions';
+} from '@/actions';
 import { ITodosProps } from '../types/ITodosRequest';
 import { IRootState } from '../types/RootState';
 
